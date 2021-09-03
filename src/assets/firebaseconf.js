e5c7bf901520bb2d59d0e591db9cbbf552987a85
Well adapted bread <56123871+RedTemi@users.noreply.github.com>
2021-09-03 22:25:41 +0100

    Update firebaseconf.js

--- src/assets/firebaseconf.js
+++ src/assets/firebaseconf.js
@@ -3,12 +3,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_xXoqwpltNkwPsTufU0kkfTgINw47_YI",
-    authDomain: "apf2021-online.firebaseapp.com",
-    projectId: "apf2021-online",
-    storageBucket: "apf2021-online.appspot.com",
-    messagingSenderId: "418672401289",
-    appId: "1:418672401289:web:cb85629ebe5eb2b5b7df58"
+    //killed for the glory of the king.
   };
   // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
